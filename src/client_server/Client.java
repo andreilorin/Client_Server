ef388eced1df6e171307306c7247737a58002a23
andrei lorin <andreilorin@gmail.com>
2017-04-19 22:40:37 +0100

    chat app

--- src/client_server/Client.java
+++ src/client_server/Client.java
@@ -6,12 +6,28 @@ import java.net.*;
 public class Client {
     
     private static final int PORT = 4141;
-    private static final String IP = "xxx.xxx.xxx.x";
+    private static final String IP = "localhost";
 
     public static void main(String[] args) {
-        try(Socket s = new Socket(InetAddress.getByName(IP), PORT)){
-            BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            System.out.println(input.readLine());
+        try(    Socket socket = new Socket(InetAddress.getByName(IP), PORT);
+                DataOutputStream dout = new DataOutputStream((socket.getOutputStream()));
+                DataInputStream din = new DataInputStream(socket.getInputStream());
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        ){
+            
+            System.out.println("Write something to the server: \n");
+            
+            String received = "", sent = "";
+            
+            while (!received.equals("end")) {
+                received = din.readUTF();
+                System.out.println(received);
+                sent = input.readLine();
+                dout.writeUTF(sent);
+                dout.flush();                
+            }
+            //socket.close();
+            
         }catch(IOException e) {
             e.printStackTrace();
         }
--- src/client_server/Server.java
+++ src/client_server/Server.java
@@ -10,14 +10,31 @@ public class Server {
     public static void main(String[] args) {
         System.out.println("Listening on port " + PORT + " CTRL + C to stop");
         
-        try(ServerSocket serverSocket = new ServerSocket(PORT)){
-            while(true){
+        try(    ServerSocket serverSocket = new ServerSocket(PORT);
                 Socket socket = serverSocket.accept();
+                //PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+                DataOutputStream dout = new DataOutputStream((socket.getOutputStream()));
+                DataInputStream din = new DataInputStream(socket.getInputStream());
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                ){
+            
+                
                 System.out.println("Connected !");
-                try(PrintWriter out = new PrintWriter(socket.getOutputStream(), true)){
-                    out.println("Hello from the server socket !");
-                }
-            }
+               
+                System.out.println("Hello from the server socket !");
+                    
+                String received = "", sent = "";
+                    
+                while(!received.equals("end")) {
+                        received = din.readUTF();
+                        System.out.println(received);
+                        sent = input.readLine();
+                        dout.writeUTF(sent);
+                        dout.flush();
+                    }
+                    //socket.close();
+                
+            
         }catch(IOException e) {
             e.printStackTrace();
         }
