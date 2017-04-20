eb47dea2384f2b0857e4c4a285ef627402a88803
andrei lorin <andreilorin@gmail.com>
2017-04-20 22:15:18 +0100

    Server rewrite

--- src/client_server/Server.java
+++ src/client_server/Server.java
@@ -4,39 +4,47 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
-    
+
     private static final int PORT = 4141;
-    
+
     public static void main(String[] args) {
-        System.out.println("Listening on port " + PORT + " CTRL + C to stop");
-        
-        try(    ServerSocket serverSocket = new ServerSocket(PORT);
+
+        Server server = new Server();
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                System.out.println("Listening on port 4141, CRTL-C to quit ");
                 Socket socket = serverSocket.accept();
-                //PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-                DataOutputStream dout = new DataOutputStream((socket.getOutputStream()));
-                DataInputStream din = new DataInputStream(socket.getInputStream());
-                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                ){
-            
-                
-                System.out.println("Connected !");
-               
-                System.out.println("Hello from the server socket !");
-                    
-                String received = "", sent = "";
-                    
-                while(!received.equals("end")) {
-                        received = din.readUTF();
-                        System.out.println(received);
-                        sent = input.readLine();
-                        dout.writeUTF(sent);
-                        dout.flush();
+                try (PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+                        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
+                    System.out.println("Connected");
+                    String received = "";
+                    while ((received = input.readLine()) != null) {
+                        System.out.println("Received: " + received);
+                        if (received.equals("hi")) {
+                            server.write(output, "hi there");
+                        }
+                        if (received.equals("bye")) {
+                            server.write(output, "see you");
+                            break;
+                        }
+                        if (received.contains("how are you")) {
+                            server.write(output, "fine, thanks!");
+                        }
                     }
-                    //socket.close();
-                
-            
-        }catch(IOException e) {
+                    System.out.print("Closing connection\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private void write(PrintWriter output, String message) {
+        System.out.println("Sending: " + message);
+        output.println(message);
+    }
+
 }
