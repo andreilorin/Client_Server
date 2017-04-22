47b23ac8b7c24926ada5ab60e67fbbf7420e1d7c
andrei lorin <andreilorin@gmail.com>
2017-04-22 22:11:57 +0100

    Client rewrite

--- src/client_server/Client.java
+++ src/client_server/Client.java
@@ -4,32 +4,43 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
-    
+
     private static final int PORT = 4141;
     private static final String IP = "localhost";
 
     public static void main(String[] args) {
-        try(    Socket socket = new Socket(InetAddress.getByName(IP), PORT);
-                DataOutputStream dout = new DataOutputStream((socket.getOutputStream()));
-                DataInputStream din = new DataInputStream(socket.getInputStream());
-                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        ){
-            
-            System.out.println("Write something to the server: \n");
-            
-            String received = "", sent = "";
-            
-            while (!received.equals("end")) {
-                received = din.readUTF();
-                System.out.println(received);
-                sent = input.readLine();
-                dout.writeUTF(sent);
-                dout.flush();                
+        if (args.length != 1) {
+            System.out.println("Must include a command line argument, try again.");
+            return;
+        }
+        
+        Client client = new Client();
+        
+        try (Socket socket = new Socket(InetAddress.getByName(IP), PORT);
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
+            client.write(output, args[0]);
+            String in = "";
+            while ((in = input.readLine()) != null) {
+                System.out.println("Received: " + in);
+                if (in.equals("hi there")) { 
+                    client.write(output, "how are you");                    
+                }
+                if (in.equals("busy")) {
+                    break;
+                }
+                if (in.contains("fine, thanks!")) {
+                    client.write(output, "nice");
+                }
             }
-            //socket.close();
-            
-        }catch(IOException e) {
+            System.out.print("Closing socket.");
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    void write(PrintWriter output, String message) {
+        System.out.println("Sending: " + message);
+        output.println(message);
     }
 }
